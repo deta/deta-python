6216b7e938b1f24d60f783f95a7b898ccaa71fef
Mweya Ruider <mweya@orcachat.com>
2021-04-09 12:10:52 +0200

    setup: Switched to incremental for versioning

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup
 
 setup(
     name="deta",
-    version="0.7",
+    use_incremental=True,
+    setup_requires=['incremental'],
+    install_requires=['incremental'],
     description="Deta lib for Python 3",
     url="http://github.com/deta/deta-python",
-    long_description="Deta SDK for Python",
+    long_description="Deta SDK for python. Documentation available at https://docs.deta.sh/docs/base/sdk.",
     repository="https://test.pypi.org/legacy/",
     author="Deta",
     author_email="hello@deta.sh",
