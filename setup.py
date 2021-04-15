7e5b570092fd9f63cadc098596ee8cdd9b8cef98
yankeexe <yankee.exe@gmail.com>
2021-04-15 20:34:00 +0545

    Update: SDK packaging

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
-from setuptools import setup
+"""
+Packaging for the SDK
+"""
+import setuptools
 
-setup(
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+requirements = ["python-dotenv"]
+
+setuptools.setup(
     name="deta",
     version="0.4",
     description="Deta lib for Python 3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="http://github.com/deta/deta-python",
     author="Deta",
     author_email="hello@deta.sh",
     license="MIT",
+    packages=setuptools.find_packages(exclude=["dist", "build", "*.egg-info", "tests"]),
+    install_requires=requirements,
+    extras_require={"dev": ["black", "setuptools", "wheel", "twine", "mypy"]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=["deta"],
 )
