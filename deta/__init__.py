c9993a2d777009a53edd78438e6d4966bbec68ad
Mweya Ruider <mweyaruider@gmail.com>
2021-04-16 16:12:08 +0200

    setup, init: version bump to 0.7

--- deta/__init__.py
+++ deta/__init__.py
@@ -16,7 +16,7 @@ try:
 except Exception:
     pass
 
-__version__ = 0.4
+__version__ = 0.7
 
 class Deta:
     def __init__(self, project_key: str = None, *, project_id: str = None, host: str = None):
