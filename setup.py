49a0bda91677fa2987ec77d83e352ea451132b8e
Aavash Shrestha <aavash09@gmail.com>
2021-06-28 14:22:37 +0200

    bump release version to '0.9'

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="deta",
-    version="0.8",
+    version="0.9",
     description="Python SDK for Deta Base & Deta Drive.",
     url="http://github.com/deta/deta-python",
     author="Deta",
