7f0a01e0539c578d3768dfeea04a540ceb7961ce
Aavash Shrestha <aavshr@gmail.com>
2023-07-18 17:34:27 +0200

    release 1.2.0 (#91)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="deta",
-    version="1.1.0",
+    version="1.2.0",
     description="Python SDK for Deta Base & Deta Drive.",
     url="http://github.com/deta/deta-python",
     author="Deta",
